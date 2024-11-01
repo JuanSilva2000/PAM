5762d99806286bd5a6676e9b193320d6741938ea
PIERO PILCO REYNOSO <piero.pilco.r@uni.pe>
2024-11-01 01:48:55 -0500

    aside complete

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from "express"
 import cors from "cors"
 import morgan from "morgan"
+import router from "./routes/recipes.routes.js"
 
 const app = express()
 
@@ -8,6 +9,7 @@ app.use(cors())
 app.use(morgan('dev'))
 app.use(express.json())
 
+app.use(router)
 
 const port = 3000
 app.listen(port, ()=>{
